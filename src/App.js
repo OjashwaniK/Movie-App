cc0c5a69d14eac88186b1bf4243d3868d4387a38
ojashwanik <katiyarojashwani12345@gmail.com>
2022-05-02 18:19:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,69 @@
+
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import MovieCard from "./MovieCard";
+import "./App.css";
+const IMG_API = "https://image.tmdb.org/t/p/w400";
+const MOVIE_URL =
+  "http://api.themoviedb.org/3/discover/movie?sortby=popularity.disc&api_key=ea2aaad1ccaae5d86274b9d7f1e5211d";
+
+const SEARCH_URL =
+  "http://api.themoviedb.org/3/search/movie?api_key=ea2aaad1ccaae5d86274b9d7f1e5211d&query=";
 
 function App() {
-  const featured_Api =
-    " https://imdb-api.com/API/AdvancedSearch/k_pl6t3s12/?genres=action,adventure";
-
- 
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const getMovies = async () => {
-    const response = await fetch(featured_Api);
+
+  const movieRequest = async () => {
+    const response = await fetch(MOVIE_URL);
     const responseJson = await response.json();
-    console.log(responseJson);
+    console.log(responseJson.results);
+
     setMovies(responseJson.results);
   };
 
   useEffect(() => {
-    getMovies();
+    movieRequest();
   }, []);
 
-  return (
-    <div className="container">
-      <input type="text" placeholder="Search Your Movie" />
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const submitHandler=(e)=>{
+    e.preventDefault();
+   
+    if(searchTerm){
+      fetch(SEARCH_URL+searchTerm)
+      .then((res)=>res.json())
+      .then((data)=>{
+        console.log(data);
+         setMovies(data.results)
+      })
+      setSearchTerm('');
+    }
+   
+   
+  }
 
-      <div className="moviecard_container">
+  return (
+    <>
+      <div className="search_tab">
+        <h2>Search Movie Name</h2>
+        <form onSubmit={submitHandler}>
+          <input
+            value={searchTerm}
+            onChange={handleOnChange}
+            type="text"
+            placeholder="search movie"
+          />
+        </form>
+      </div>
+      <div className="container">
         {movies.map((movie) => (
           <MovieCard {...movie} />
         ))}
       </div>
-    </div>
+    </>
   );
-
-
-
-//code check
-
-
- }
+}
 
 export default App;
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,75 +1,51 @@
 import React from "react";
-import './MovieCard.css'
+import "./MovieCard.css";
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 
-function MovieCard({ image, title, description, genres, imDbRating }) {
+
+
+function Card({ poster_path, original_title, release_date, vote_average }) {
+ const IMG_API = "https://image.tmdb.org/t/p/w400";
   return (
-    <div className="moviecard">
-      <div className="moviecard_img">
-        <img src={image} alt="" />
+    <div className="card">
+      <div className="card_item">
+        <img src={IMG_API + poster_path} alt={original_title} />
       </div>
-      <div className="moviecard_info">
-        <div className="moviecardinfo_left">
-          <h4>{title}</h4>
-          <h4>Released Year:{description}</h4>
+      <div className="card_info">
+        <div className="card_info1">
+          <h3>{original_title}</h3>
         </div>
-        <div className="moviecardinfo_right">
-       
-          <h4>{imDbRating}</h4>
+
+        <div className="card_info2">
+          <StarBorderIcon/>
+          <p>{vote_average}</p>
         </div>
       </div>
     </div>
   );
 }
 
-export default MovieCard;
-// {
-//     "id": "tt11138512",
-//     "image": "https://imdb-api.com/images/original/MV5BMzVlMmY2NTctODgwOC00NDMzLWEzMWYtM2RiYmIyNTNhMTI0XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_Ratio0.6837_AL_.jpg",
-//     "title": "The Northman",
-//     "description": "(2022)",
-//     "runtimeStr": "137 min",
-//     "genres": "Action, Adventure, Drama",
-//     "genreList": [
-//         {
-//             "key": "Action",
-//             "value": "Action"
-//         },
-//         {
-//             "key": "Adventure",
-//             "value": "Adventure"
-//         },
-//         {
-//             "key": "Drama",
-//             "value": "Drama"
-//         }
-//     ],
-//     "contentRating": "R",
-//     "imDbRating": "7.8",
-//     "imDbRatingVotes": "37357",
-//     "metacriticRating": "82",
-//     "plot": "From visionary director Robert Eggers comes The Northman, an action-filled epic that follows a young Viking prince on his quest to avenge his father's murder.",
-//     "stars": "Robert Eggers, Alexander Skarsgård, Nicole Kidman, Claes Bang, Ethan Hawke",
-//     "starList": [
-//         {
-//             "id": "tt11138512",
-//             "name": "Robert Eggers"
-//         },
-//         {
-//             "id": "tt11138512",
-//             "name": "Alexander Skarsgård"
-//         },
-//         {
-//             "id": "tt11138512",
-//             "name": "Nicole Kidman"
-//         },
-//         {
-//             "id": "tt11138512",
-//             "name": "Claes Bang"
-//         },
-//         {
-//             "id": "tt11138512",
-//             "name": "Ethan Hawke"
-//         }
-//     ]
-// }
+export default Card;
+// original_language: "en"
+// original_title: "The Batman"
+// overview: "In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler."
+// popularity: 9031.638
+// poster_path: "/74xTEgt7R36Fpooo50r9T25onhq.jpg"
+// release_date: "2022-03-01"
+// title: "The Batman"
+// video: false
+// vote_average: 7.8
+// vote_count: 4043
+
+// Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_.jpg"
+// Title: "Batman"
+// Year: "1989"
+// imdbID: "tt0096895"
+
+// "id": "tt10698680",
+// "image": "https://imdb-api.com/images/original/MV5BMjMwMDgyOGQtMWZjNC00MDUwLTllZDYtZWM3NDBmN2YzNGZmXkEyXkFqcGdeQXVyMTQzNjkzMzEw._V1_Ratio0.6837_AL_.jpg",
+// "title": "K.G.F: Chapter 2",
+// "description": "(2022)",
+// "runtimeStr": "168 min",
+// "genres": "Action, Crime, Drama",
